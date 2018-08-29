aa479209fba68939a9fa666eef14ffe881644660
逸白 <zheming.bzm@alibaba-inc.com>
2018-08-29 23:46:09 +0800

    s

---
+++ app/controller/home.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+
+class HomeController extends Controller {
+  async index() {
+    const {
+      ctx
+    } = this;
+    let keyword = encodeURIComponent(ctx.params.key);
+    let url = `https://maps.googleapis.com/maps/api/place/autocomplete/json?key=AIzaSyCFXnELZRjlL9KHLXH3p4UE52jYCgD9Tto&input=${keyword}`
+
+    let res;
+
+    try{
+      res = await ctx.curl(url, {
+        method: 'GET',
+        dataType: 'json',
+        data: {},
+        timeout: 3000,
+      });
+    }catch(e){
+
+    }
+
+    console.log('==========res', res);
+    ctx.body = {
+      success: true,
+      msg: 'ok!',
+      data: {
+
+      },
+    };
+  }
+}
+
+module.exports = HomeController;
